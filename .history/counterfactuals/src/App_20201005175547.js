58faf9a1d1f1114148094ec40617d1370576ab13
Vidal Suchecky <sucheckyvidal@gmail.com>
2020-10-05 17:59:40 +0200

    created components and lib folder

--- .history/counterfactuals/src/App_20201005175547.js
+++ .history/counterfactuals/src/App_20201005175547.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="container">counterfactuals</div>;
+  return <div className=""></div>;
 }
 
 export default App;
